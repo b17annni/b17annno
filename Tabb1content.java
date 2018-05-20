186c4c0b9355a9059545a33eabf9d18d18a6063a
b17annni <b17annni@gmail.com>
2018-05-20 16:50:00 +0200

    Initial commit

---
+++ Tabb1content.java
@@ -0,0 +1,22 @@
+package com.example.b17annni.woff;
+
+/**
+ * Created by b17annni on 2018-05-20.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tabb1content extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tabb1, container, false);
+
+        return rootView;
+    }
+}
---
+++ Tabb3content.java
@@ -0,0 +1,21 @@
+package com.example.b17annni.woff;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by b17annni on 2018-05-20.
+ */
+
+public class Tabb3content extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tabb3, container, false);
+
+        return rootView;
+    }
+}
